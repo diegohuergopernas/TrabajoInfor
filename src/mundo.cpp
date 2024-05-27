c423117f9dd38b1443dcad6636fc0073b1aad4a4
diegohuergopernas <diego.huergo.pernas@alumnos.upm.es>
2024-05-27 23:16:26 +0200

    ento de arreglar error de freeglut

--- src/mundo.cpp
+++ src/mundo.cpp
@@ -2,7 +2,7 @@
 
 void Mundo::dibujaMundo()
 {
-	gluLookAt(0, 7.5, 20,  // posicion del ojo
+	gluLookAt(0, 0, 0,  // posicion del ojo
 		0.0, 7.5, 0.0,      // hacia que punto mira  (0,0,0) 
 		0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y) 
 	tablero.dibujarTablero();
