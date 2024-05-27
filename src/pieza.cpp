9a7c38feca3083d8b6c10ecb411d8ac20213c6c5
diegohuergopernas <diego.huergo.pernas@alumnos.upm.es>
2024-05-27 21:25:00 +0200

    Inicio clase Pieza Padre del resto de piezas

---
+++ src/pieza.cpp
@@ -0,0 +1,17 @@
+#include "pieza.h"
+
+Pieza::Pieza(Tipo tipo, Color color, Coordenadas coordenadas)
+{
+}
+
+void Pieza::SetColor()
+{
+}
+
+void Pieza::SetCoordenadas()
+{
+}
+
+void Pieza::SetTipo()
+{
+}
--- src/pieza.h
+++ src/pieza.h
@@ -1,6 +1,25 @@
 #pragma once
 
 enum Tipo { REY, REINA, ALFIL, TORRE, CABALLO, PEON };
+enum Color { NEGRO, BLANCO };
 class Pieza {
+
+	Color color;
+	Tipo tipo;
+	Coordenadas coordenadas;
+
+public:
+	Pieza(Tipo tipo, Color color, Coordenadas coordenadas);
+	virtual ~Pieza();
+
+	//Funciones comunes a todas las piezas
+	virtual void dibujarPieza() = 0; //virtual puro para especializar dibuja en cada pieza
+	virtual void moverPieza(int fila, int columna, Pieza*) = 0; //virtual puro para especializar movimiento de cada pieza
+	virtual void killPieza() = 0;//funcion matar
+
+	void SetColor();
+	void SetCoordenadas();
+	void SetTipo();
+
 
 };
