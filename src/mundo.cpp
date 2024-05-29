7ce8723cc64d5a402cf11e060114ab2977fd7cc2
diegohuergopernas <diego.huergo.pernas@alumnos.upm.es>
2024-05-30 00:37:45 +0200

    Ajustar vista en funcion del tablero

--- src/mundo.cpp
+++ src/mundo.cpp
@@ -1,15 +1,26 @@
 #include "mundo.h"
 #include "pieza.h"
-
+#include "math.h"
 
 void Mundo::dibujaMundo()
 {
-	//piezas.agregar(new Reina);//prueba agregar reina a la pieza
-	gluLookAt(6, 6, 20,  // posicion del ojo
-		6, 6, 0.0,      // hacia que punto mira  (0,0,0) 
-		0.0, 1.0, 0);      // definimos hacia arriba (eje Y) 
-	tablero.dibujarTablero();
-	piezas.dibuja();
-	
+    int filas = tablero.getFilas();
+    int columnas = tablero.getColumnas();
+    float casillaSize = tablero.getCasillaSize();
+
+    // Calcula el centro del tablero
+    float centerX = columnas * casillaSize / 2.0f;
+    float centerY = filas * casillaSize / 2.0f;
+
+    // Ajusta la posición de la cámara según el tamaño del tablero
+    float eyeX = centerX;
+    float eyeY = centerY;
+    float eyeZ = (filas > columnas ? filas : columnas) * casillaSize;  // La distancia de la cámara depende del tamaño del tablero
+
+    gluLookAt(eyeX, eyeY, eyeZ + 7,  // Posición del ojo
+        centerX, centerY, 0.0,      // Hacia qué punto mira
+        0.0, 1.0, 0);      // Definimos hacia arriba (eje Y)
+    tablero.dibujarTablero();
+    //pieza.dibujarPieza();
 
 }
--- src/tablero.h
+++ src/tablero.h
@@ -10,6 +10,12 @@ class Tablero {
 public:
 
 	Tablero(int f = 8, int c = 8, float size = 1.5f) : filas(f), columnas(c), casillaSize(size) {}
-	
+
 	void dibujarTablero();
+
+	int getFilas() const { return filas; }
+	int getColumnas() const { return columnas; }
+	float getCasillaSize() const {
+		return casillaSize;
+	}
 };
