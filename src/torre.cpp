f8f6a8afc6a97c260313301db5ef63f2d7fd641c
umartinezr <u.martinezr@alumnos.upm.es>
2024-05-30 20:59:09 +0200

    prueba subida torrecpp

--- src/torre.cpp
+++ src/torre.cpp
@@ -1,1 +1,8 @@
+#include "torre.h"
 
+Torre::Torre(Color color, Coordenadas coord) :Pieza(color, coord, TORRE) {}
+
+void Torre::dibuja() {
+	Sprite spriteNeg{ "bin/imagenes/torreneg.png" };
+	Sprite spriteBlc{ "bin/imagenes/torreblc.png" };
+}
